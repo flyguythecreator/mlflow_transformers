71c114822313416b14c231a86a51a1f6dd19b145
Dillon Ragin <dillonragin@Dillons-MacBook-Pro.local>
2025-04-02 19:24:02 -0400

    Logging the Prompt Template Model with MLFlow and updating the open sourced fine-tuned model folder name.

--- prompt_templating/index.py
+++ prompt_templating/index.py
@@ -1,4 +1,6 @@
 from transformers import pipeline
+import mlflow
+
 
 generator = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T")
 
@@ -12,8 +14,8 @@ prompt_templates = [
   # dialogue style template with a system prompt
   (
       "You are an assistant that is knowledgeable about birds. "
-      "If asked about the largest bird, you will reply 'Duck'."
-      "User: {prompt}"
+      "If asked about the largest bird, you will reply 'Duck'. "
+      "User: {prompt} "
       "Assistant: "
   ),
 ]
@@ -24,3 +26,41 @@ responses = generator(
 for idx, response in enumerate(responses):
   print(f"Response to Template #{idx}:")
   print(response[0]["generated_text"] + "")
+
+# Creating a Model Signature of MLFlow Monitoring 
+sample_input = "Tell me the largest bird"
+params = {"max_new_tokens": 15}
+signature = mlflow.models.infer_signature(
+  sample_input,
+  mlflow.transformers.generate_signature_output(generator, sample_input, params=params),
+  params=params,
+)
+
+# visualize the signature
+print("Visualizing the Generated MLFlow Model Signature: ", signature)
+
+# If you are running this tutorial in local mode, leave the next line commented out.
+# Otherwise, uncomment the following line and set your tracking uri to your local or remote tracking server.
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+# Set a name for the experiment that is indicative of what the runs being created within it are in regards to
+mlflow.set_experiment("prompt-templating")
+
+prompt_template = "Q: {prompt}/n A:"
+with mlflow.start_run():
+  model_info = mlflow.transformers.log_model(
+      transformers_model=generator,
+      artifact_path="model",
+      task="text-generation",
+      signature=signature,
+      input_example="Tell me the largest bird",
+      prompt_template=prompt_template,
+      # Since MLflow 2.11.0, you can save the model in 'reference-only' mode to reduce storage usage by not saving
+      # the base model weights but only the reference to the HuggingFace model hub. To enable this, uncomment the
+      # following line:
+      save_pretrained=False,
+  )
+
+loaded_generator = mlflow.pyfunc.load_model(model_uri=model_info.model_uri)
+
+print("Loaded Model Prediction: ", loaded_generator.predict("Tell me the largest bird"))
